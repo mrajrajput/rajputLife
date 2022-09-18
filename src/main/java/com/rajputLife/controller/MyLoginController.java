ee08180883d8626cad7e91ced2fa030f044540bf
FirstCommit <Manjul@192.168.1.105>
2022-09-18 01:18:44 -0500

    Showing loggedIn user in both OAuth and Form login

--- src/main/java/com/rajputLife/controller/MyLoginController.java
+++ src/main/java/com/rajputLife/controller/MyLoginController.java
@@ -1,5 +1,6 @@
 package com.rajputLife.controller;
 
+import com.rajputLife.entity.security.User;
 import com.rajputLife.model.Product;
 import com.rajputLife.repository.ProductRepository;
 import org.ocpsoft.rewrite.annotation.Join;
@@ -8,6 +9,8 @@ import org.ocpsoft.rewrite.el.ELBeanName;
 import org.ocpsoft.rewrite.faces.annotation.Deferred;
 import org.ocpsoft.rewrite.faces.annotation.IgnorePostback;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
@@ -37,7 +40,13 @@ public class MyLoginController {
 		products = productRepository.findAll();
 	}
 
-	public List<Product> getProducts() {
+	public String getLoggedUser() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if(auth.getName().contains("anonymousUser")) return null;
+		return auth.getName(); //get logged in username
+	}
+
+		public List<Product> getProducts() {
 		return products;
 	}
 
