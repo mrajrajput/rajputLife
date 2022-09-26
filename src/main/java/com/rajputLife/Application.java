263acb72cf17bc79c42a90c463fda2033d51d6b2
FirstCommit <Manjul@192.168.1.105>
2022-09-26 00:22:01 -0500

    barely working with /s/dashboard.jsf

--- src/main/java/com/rajputLife/Application.java
+++ src/main/java/com/rajputLife/Application.java
@@ -3,10 +3,23 @@ package com.rajputLife;
 //import javax.faces.webapp.FacesServlet;
 //import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import com.rajputLife.repository.ProductRepository;
+import com.sun.faces.config.ConfigureListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import javax.faces.webapp.FacesServlet;
+import java.util.Locale;
 
 @EnableAutoConfiguration
 //@ComponentScan({"com.auth0.samples.bootfaces"})
@@ -24,11 +37,51 @@ import org.springframework.context.annotation.ComponentScan;
  		"com.rajputLife.repository.security"}
 
 			)
-public class Application { //extends SpringBootServletInitializer {
+public class Application  extends WebMvcConfigurerAdapter { //extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+
+	@Bean
+	public ServletRegistrationBean facesServletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean<>(new FacesServlet(), "*.xhtml");
+		registration.setLoadOnStartup(1);
+		return registration;
+	}
+
+	@Bean
+	public ServletContextInitializer servletContextInitializer() {
+		return servletContext -> {
+			servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+//            servletContext.setInitParameter("primefaces.THEME", "omega-light");
+		};
+	}
+
+	@Bean
+	public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+		return new ServletListenerRegistrationBean<>(new ConfigureListener());
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("ns/login.xhtml");
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.US);
+		return slr;
+	}
+
+
 
 //	@Bean
 //	public ServletRegistrationBean servletRegistrationBean() {
